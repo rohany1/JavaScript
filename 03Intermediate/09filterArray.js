8b6b7e5c99dbe95e86b732ece07ffb332d2f04c6
Rohan <rohany.hackerank1@gmail.com>
2021-07-26 12:00:17 +0530

    Fill and Filter Function learned

---
+++ 03Intermediate/09filterArray.js
@@ -0,0 +1,7 @@
+var testArray=[2,4,5,2,5,1,7,6];
+console.log(testArray.fill("empty"));
+
+const myNumber=[23,24,25,55,66,77,87,98];
+
+const result=myNumber.filter((num)=>(num <=55));
+console.log(result);
